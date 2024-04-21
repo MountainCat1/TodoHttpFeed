fd2aeee5a7232dd4367a7c7d38040abb19f37c00
Mountain Cat <tomasz.banas123@gmail.com>
2024-04-21 13:42:28 +0200

    add cmd app

---
+++ CmdApp/CmdApp/Program.cs
@@ -0,0 +1,17 @@
+﻿while (true)
+{
+    using (var client = new HttpClient())
+    {
+        try
+        {
+            var response = await client.GetAsync("http://localhost:5000/api/todoitems");
+            var content = await response.Content.ReadAsStringAsync();
+            File.WriteAllText("ToDoItemsCurrentSnapshot.json", content);    
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    Thread.Sleep(10000);
+}
